6b81fb7036bbb160791de6805d8a5d1a3da22872
Daniel <irfan2904.ia@gmail.com>
2025-09-21 13:21:14 +0700

    update runApp materialApp

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(LoginScreen());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginScreen(),
+  ));
 }
 
 class LoginScreen extends StatelessWidget {
